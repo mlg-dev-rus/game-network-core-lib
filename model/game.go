69be7b4bd77a12ddfc5f2a6bafa6382b3119bdc4
mike-barshev <mike.barshev@gmail.com>
2021-06-09 23:09:40 +0300

    add bson tag add toBigModel func add func for array models

--- model/game.go
+++ model/game.go
@@ -2,63 +2,65 @@ package model
 
 //Game - game information
 type Game struct {
-	ID              string            `json:"id"`
-	Name            string            `json:"name"`
-	NameTranslation map[string]string `json:"translation_name"`
-	Type            string            `json:"type"`
-	DLC             []int             `json:"dlc"`
-	Platforms       map[string]bool   `json:"platforms"`
+	ID              string            `json:"id" bson:"id"`
+	Name            string            `json:"name" bson:"name"`
+	NameTranslation map[string]string `json:"translation_name" bson:"translation_name"`
+	Type            string            `json:"type" bson:"type"`
+	MainGame        int               `bson:"main_game" bson:"main_game"`
+	DLC             []int             `json:"dlc" bson:"dlc"`
+	Platforms       map[string]bool   `json:"platforms" bson:"platforms"`
 	ReleaseDate     struct {
-		ComingSoon bool `json:"coming_soon"`
-		Date       int  `json:"date"`
-	} `json:"release_date"`
+		ComingSoon bool `json:"coming_soon" bson:"coming_soon"`
+		Date       int  `json:"date" bson:"date"`
+	} `json:"release_date" bson:"release_date"`
 	Categories []struct {
-		ID          string `json:"id"`
-		Description string `json:"description"`
+		ID          string `json:"id" bson:"id"`
+		Description string `json:"description" bson:"description"`
 	} `json:"categories"`
 	Genres []struct {
-		ID          string `json:"id"`
-		Description string `json:"description"`
+		ID          string `json:"id" bson:"id"`
+		Description string `json:"description" bson:"description"`
 	} `json:"genres"`
-	AboutTheGame     string   `json:"about_the_game"`
-	ShortDescription string   `json:"short_description"`
-	Website          string   `json:"website"`
-	Developers       []string `json:"developers"`
+	AboutTheGame     string   `json:"about_the_game" bson:"about_the_game"`
+	ShortDescription string   `json:"short_description" bson:"short_description"`
+	Website          string   `json:"website" bson:"website"`
+	Developers       []string `json:"developers" bson:"developers"`
 	Metacritic       struct {
-		Score int    `json:"score"`
-		URL   string `json:"url"`
-	} `json:"metacritic"`
-	StoreIds map[string]string `json:"store_ids"`
+		Score int    `json:"score" bson:"score"`
+		URL   string `json:"url" bson:"url"`
+	} `json:"metacritic" bson:"metacritic"`
+	StoreIds map[string]string `json:"store_ids" bson:"store_ids"`
 }
 
 type GameSmall struct {
-	ID              string            `json:"id"`
-	Name            string            `json:"default_name"`
-	NameTranslation map[string]string `json:"translation_name"`
-	Type            string            `json:"type"`
-	DLC             []int             `json:"dlc"`
-	Platforms       []string          `json:"platforms"`
+	ID              string            `json:"id" bson:"id"`
+	Name            string            `json:"default_name" bson:"name"`
+	NameTranslation map[string]string `json:"translation_name" bson:"name_translation"`
+	Type            string            `json:"type" bson:"type"`
+	MainGame        int               `bson:"main_game" bson:"main_game"`
+	DLC             []int             `json:"dlc" bson:"dlc"`
+	Platforms       []string          `json:"platforms" bson:"platforms"`
 	ReleaseDate     struct {
-		ComingSoon bool `json:"coming_soon"`
-		Date       int  `json:"date"`
-	} `json:"release_date"`
+		ComingSoon bool `json:"coming_soon" bson:"coming_soon"`
+		Date       int  `json:"date" bson:"date"`
+	} `json:"release_date" bson:"release_date"`
 	Categories []struct {
-		ID          string `json:"id"`
-		Description string `json:"description"`
-	} `json:"categories"`
+		ID          string `json:"id" bson:"id"`
+		Description string `json:"description" bson:"description"`
+	} `json:"categories" bson:"categories"`
 	Genres []struct {
-		ID          string `json:"id"`
-		Description string `json:"description"`
-	} `json:"genres"`
-	AboutTheGame     string   `json:"about_the_game"`
-	ShortDescription string   `json:"short_description"`
-	Website          string   `json:"website"`
-	Developers       []string `json:"developers"`
+		ID          string `json:"id" bson:"id"`
+		Description string `json:"description" bson:"description"`
+	} `json:"genres" bson:"genres"`
+	AboutTheGame     string   `json:"about_the_game" bson:"about_the_game"`
+	ShortDescription string   `json:"short_description" bson:"short_description"`
+	Website          string   `json:"website" bson:"website"`
+	Developers       []string `json:"developers" bson:"developers"`
 	Critics          []struct {
-		ID    string `json:"id"`
-		Name  string `json:"name"`
-		Score int    `json:"score"`
-		URL   string `json:"url"`
-	} `json:"critics"`
-	StoreIds map[string]string `json:"store_ids"`
+		ID    string `json:"id" bson:"id"`
+		Name  string `json:"name" bson:"name"`
+		Score int    `json:"score" bson:"score"`
+		URL   string `json:"url" bson:"url"`
+	} `json:"critics" bson:"critics"`
+	StoreIds map[string]string `json:"store_ids" bson:"store_ids"`
 }
--- utils/model_transfer.go
+++ utils/model_transfer.go
@@ -4,6 +4,10 @@ import (
 	. "github.com/mlg-dev-rus/game-network-core-lib/model"
 )
 
+const Windows = "Windows"
+const Linux = "Linux"
+const Mac = "Mac"
+
 func GameToSmallModel(src Game) GameSmall {
 	var srcSmall GameSmall
 
@@ -25,10 +29,10 @@ func GameToSmallModel(src Game) GameSmall {
 	srcSmall.Website = src.Website
 	srcSmall.Developers = src.Developers
 	srcSmall.Critics = append(srcSmall.Critics, struct {
-		ID    string `json:"id"`
-		Name  string `json:"name"`
-		Score int    `json:"score"`
-		URL   string `json:"url"`
+		ID    string `json:"id" bson:"id"`
+		Name  string `json:"name" bson:"name"`
+		Score int    `json:"score" bson:"score"`
+		URL   string `json:"url" bson:"url"`
 	}{
 		"Metacritic",
 		"Metacritic",
@@ -37,4 +41,62 @@ func GameToSmallModel(src Game) GameSmall {
 	})
 	srcSmall.StoreIds = src.StoreIds
 	return srcSmall
+}
+
+func GameToBigModel(srcSmall GameSmall) Game {
+	var src Game
+
+	src.ID = srcSmall.ID
+	src.Name = srcSmall.Name
+	src.NameTranslation = srcSmall.NameTranslation
+	src.Type = srcSmall.Type
+	src.DLC = srcSmall.DLC
+	for _, value := range srcSmall.Platforms {
+		if value == Windows {
+			src.Platforms[Windows] = true
+		} else {
+			src.Platforms[Windows] = false
+		}
+		if value == Linux {
+			src.Platforms[Linux] = true
+		} else {
+			src.Platforms[Linux] = false
+		}
+		if value == Mac {
+			src.Platforms[Mac] = true
+		} else {
+			src.Platforms[Mac] = false
+		}
+	}
+	src.ReleaseDate = srcSmall.ReleaseDate
+	src.Categories = srcSmall.Categories
+	src.Genres = srcSmall.Genres
+	src.AboutTheGame = srcSmall.AboutTheGame
+	src.ShortDescription = srcSmall.ShortDescription
+	src.Website = srcSmall.Website
+	src.Developers = srcSmall.Developers
+
+	for i := 0; i < len(srcSmall.Critics); i++ {
+		src.Metacritic.Score = srcSmall.Critics[i].Score
+		src.Metacritic.URL = srcSmall.Critics[i].URL
+	}
+
+	src.StoreIds = srcSmall.StoreIds
+	return src
+}
+
+func GameToSmallModelArr(src []Game) []GameSmall {
+	var srcSmall []GameSmall
+	for i := 0; i < len(src); i++ {
+		srcSmall[i] = GameToSmallModel(src[i])
+	}
+	return srcSmall
+}
+
+func GameToBigModelArr(srcSmall []GameSmall) []Game {
+	var src []Game
+	for i := 0; i < len(srcSmall); i++ {
+		src[i] = GameToBigModel(srcSmall[i])
+	}
+	return src
 }
